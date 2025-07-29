71037628eea6d502f46aa4dddf28415a66ed10d9
Ramiro <ramiroec@gmail.com>
2025-07-29 15:44:50 -0300

    versiones para turso

--- conexionDB.js
+++ conexionDB.js
@@ -1,9 +1,9 @@
-// conexionDB.js
 const { createClient } = require('@libsql/client');
 
 const db = createClient({
-  url: 'libsql://emails-ramiroec.aws-us-east-1.turso.io', // Cambia esto por tu URL real
-  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NTM3NjAyMDYsImlkIjoiNTMwMzQ1MGItOTA0My00MjY0LTkzZmYtNzc1MjMxNWVlNmI0IiwicmlkIjoiNzg3Mzc2Y2YtYmQ2NS00MmU5LWJlY2YtNDJjY2VkMzQwMzE0In0.tSDJvlaASTx1THL99Mr9vROwLhc56_ioanRSTELI-o5ziQ6Czzeoju0TZ87j3NiPGeEFMzHO8JgbBwDbbLigBw', // Cambia esto por tu token real
+  url: 'libsql://emails-ramiroec.aws-us-east-1.turso.io',
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NTM3NjAyMDYsImlkIjoiNTMwMzQ1MGItOTA0My00MjY0LTkzZmYtNzc1MjMxNWVlNmI0IiwicmlkIjoiNzg3Mzc2Y2YtYmQ2NS00MmU5LWJlY2YtNDJjY2VkMzQwMzE0In0.tSDJvlaASTx1THL99Mr9vROwLhc56_ioanRSTELI-o5ziQ6Czzeoju0TZ87j3NiPGeEFMzHO8JgbBwDbbLigBw',
+  syncUrl: undefined, // 👈 esto evita que cargue sqlite local
 });
 
 module.exports = db;
